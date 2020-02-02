f7b3a09bc29a3d938993c744ebb0c54051d23cb4
Heberth Teofilo da Silva <38838887+heberthteofilo@users.noreply.github.com>
2020-02-02 10:51:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao(View v){
+        TextView texto = findViewById(R.id.texto);
+        texto.setText("TEXTO ALTERADO COM SUCESSO");
+
+    }
+}
